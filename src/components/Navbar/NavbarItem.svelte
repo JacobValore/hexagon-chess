d7f36539a8c6346aff336f0b1f9855d8256681f0
JacobValore <jacobvalore@gmail.com>
2023-07-29 09:30:20 -0600

    created route structure

---
+++ src/components/Navbar/NavbarItem.svelte
@@ -0,0 +1,57 @@
+<script>
+	import { goto } from '$app/navigation';
+	export let name;
+	export let isActive;
+	export let classStyle;
+	export let link = '';
+
+	let element;
+
+	const setActive = () => {
+		if (window.active === element) return;
+		window.active?.classList.remove('tabItemActive');
+		element.classList.add('tabItemActive');
+		window.active = element;
+	};
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+	bind:this={element}
+	on:click={() => {
+		goto(`/${link}`);
+		setActive();
+	}}
+	class={isActive === true ? 'tabItem tabItemActive' : 'tabItem'}
+>
+	<i class="{classStyle} sidebarIcon" />
+	<span>{name}</span>
+</div>
+
+<style>
+	.tabItem {
+		flex-shrink: 0;
+
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: start;
+		padding: 10px;
+		border-radius: 5px;
+		gap: 10px;
+	}
+
+	.tabItem:hover {
+		background-color: #19191d;
+		transition: all 0.1s linear;
+		cursor: pointer;
+	}
+
+	.tabItemActive {
+		background-color: #19191d;
+	}
+
+	.sidebarIcon {
+		width: 20px;
+	}
+</style>
---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,25 @@
+<div class="app-content">
+    <div class="app-sidebar">
+        Sidebar Text
+    </div>
+    <div class="app-page">
+        <slot />
+    </div>
+</div>
+
+<style>
+    .app-content{
+        display: flex;
+        flex-direction: row;
+        height: 100%;
+
+        /* background: var(); */
+    }
+    .app-sidebar{
+        flex-shrink: 0;
+        width: 150px;
+    }
+    .app-page{
+        flex-grow: 1;
+    }
+</style>
